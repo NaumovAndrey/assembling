b4c1cefa4d1718f8a5ff0eebd8b990cf77b18cd6
andrey <naumov.a.n@icloud.com>
2024-03-11 23:30:06 +0700

    init project

---
+++ src/main/java/ru/gb/assembling/AssemblingApplication.java
@@ -0,0 +1,13 @@
+package ru.gb.assembling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssemblingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AssemblingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/gb/assembling/AssemblingApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.gb.assembling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssemblingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
